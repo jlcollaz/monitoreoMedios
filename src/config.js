a9733a9ec937db55fbadd62b9e8e204b3bad75c1
jorgecollazos <jorge.collazos@utec.edu.pe>
2021-09-28 13:24:00 -0500

    db

--- src/config.js
+++ src/config.js
@@ -7,10 +7,10 @@ config();
 export default {
   database: {
     connectionLimit: 10,
-    host: process.env.DATABASE_HOST || "localhost",
-    user: process.env.DATABASE_USER || "root",
-    password: process.env.DATABASE_PASSWORD || "root",
-    database: process.env.DATABASE_NAME || "db_MM",
+    host: process.env.DATABASE_HOST || "us-cdbr-east-04.cleardb.com",
+    user: process.env.DATABASE_USER || "b239329977702e",
+    password: process.env.DATABASE_PASSWORD || "94e963d4",
+    database: process.env.DATABASE_NAME || "heroku_84279390f2c6bba",
   },
   port: process.env.PORT || 4000,
 };
